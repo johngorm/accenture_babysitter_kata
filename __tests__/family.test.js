5d6efe1ec04d3c8e64666f404168a2f0d6cd998e
John Gorman <jack.m.gman@gmail.com>
2021-04-21 14:20:35 -0500

    Add family rate time dividers

--- __tests__/family.test.js
+++ __tests__/family.test.js
@@ -1,9 +1,21 @@
-import { expect } from '@jest/globals';
-import familyRates from '../src/families';
+import { expect, test } from '@jest/globals';
+import familyRateTimeBlocks from '../src/families';
 describe('familyRates', () => {
-  test('should return family rates for family A,B, and C', () => {
-    expect(familyRates.A.length).toEqual(2);
-    expect(familyRates.B.length).toEqual(3);
-    expect(familyRates.C.length).toEqual(2);
+  test('should have correct number of rates based on time blocks for family A,B, and C', () => {
+    expect(Object.keys(familyRateTimeBlocks.A).length).toEqual(2);
+    expect(Object.keys(familyRateTimeBlocks.B).length).toEqual(3);
+    expect(Object.keys(familyRateTimeBlocks.C).length).toEqual(2);
   });
-})
+
+  test('each family start time should have a numerical rate', () => {
+    Object.keys(familyRateTimeBlocks.A).forEach(startTime => {
+      expect(typeof familyRateTimeBlocks.A[startTime]).toEqual('number');
+    });
+    Object.keys(familyRateTimeBlocks.B).forEach(startTime => {
+      expect(typeof familyRateTimeBlocks.B[startTime]).toEqual('number');
+    });
+    Object.keys(familyRateTimeBlocks.C).forEach(startTime => {
+      expect(typeof familyRateTimeBlocks.C[startTime]).toEqual('number');
+    });
+  });
+});
--- src/families.js
+++ src/families.js
@@ -1,7 +1,17 @@
 const families = {
-  A: new Array(2),
-  B: new Array(3),
-  C: new Array(2)
+  A: {
+    "5:00PM": 15,
+    "11:00PM": 20
+  },
+  B: {
+    "5:00PM": 12,
+    "10:00PM": 8,
+    "12:00AM": 16
+  },
+  C: {
+    "5:00PM": 21,
+    "9:00PM": 15
+  }
 };
 
 export default families;
