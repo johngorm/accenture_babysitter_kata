85ddda1a3e36adcaa9c954d90565180474e824fa
John Gorman <jack.m.gman@gmail.com>
2021-04-21 23:38:15 -0500

    Validate startTime format

--- __tests__/calculateBabysittingFee.test.js
+++ __tests__/calculateBabysittingFee.test.js
@@ -1,4 +1,3 @@
-import { expect } from '@jest/globals';
 import calculateBabysittingFee from '../src/calculateBabysittingFee'
 describe('calculateBabysittingFee', () => {
   test('should exist', () => {
@@ -6,16 +5,25 @@ describe('calculateBabysittingFee', () => {
   });
 
   test('returns a number', () => {
-    const output = calculateBabysittingFee('foo', 'fiz', 'buzz');
+    const output = calculateBabysittingFee('6:00PM', 'fiz', 'buzz');
     expect(typeof output).toEqual('number');
   });
 
-  test('function requires 3 string parameters', () => {
+  test('function throws error if missing any of the three required inputs', () => {
     expect.assertions(1);
     try {
       calculateBabysittingFee();
     } catch (e) {
       expect(e.message).toEqual('Error: 3 arguments required, start time, end time and family code letter');
     }
-  })
+  });
+
+  test('functions throws error if startTime can not be parsed as appropriate format (HH:MMAM/PM)', () => {
+    expect.assertions(1);
+    try {
+      calculateBabysittingFee("asdf", "4:00PM", 'A');
+    } catch (e) {
+      expect(e.message).toEqual('Error: start time must be a time string of hours and minutes and ante or post meridiem')
+    }
+  });
 });
--- src/calculateBabysittingFee.js
+++ src/calculateBabysittingFee.js
@@ -1,7 +1,14 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
+const acceptedTimeFormat = 'h:mmA';
 const calculateBabysittingFee = (startTime, endTime, familyCode) => {
   if (startTime === undefined ||  endTime === undefined || familyCode === undefined) {
     throw new Error('Error: 3 arguments required, start time, end time and family code letter');
+  } else if (!dayjs(startTime, acceptedTimeFormat, true).isValid()) {
+    throw new Error('Error: start time must be a time string of hours and minutes and ante or post meridiem');
   }
+  
   return 0.00;
 }
 
