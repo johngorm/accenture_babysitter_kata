469210d3ced6b7dea782e779e2a3f57e08464fc4
John Gorman <jack.m.gman@gmail.com>
2021-04-21 21:52:48 -0500

    Require 3 arguments

--- __tests__/calculateBabysittingFee.test.js
+++ __tests__/calculateBabysittingFee.test.js
@@ -1,3 +1,4 @@
+import { expect } from '@jest/globals';
 import calculateBabysittingFee from '../src/calculateBabysittingFee'
 describe('calculateBabysittingFee', () => {
   test('should exist', () => {
@@ -5,7 +6,16 @@ describe('calculateBabysittingFee', () => {
   });
 
   test('returns a number', () => {
-    const output = calculateBabysittingFee();
+    const output = calculateBabysittingFee('foo', 'fiz', 'buzz');
     expect(typeof output).toEqual('number');
+  });
+
+  test('function requires 3 string parameters', () => {
+    expect.assertions(1);
+    try {
+      calculateBabysittingFee();
+    } catch (e) {
+      expect(e.message).toEqual('Error: 3 arguments required, start time, end time and family code letter');
+    }
   })
 });
--- src/calculateBabysittingFee.js
+++ src/calculateBabysittingFee.js
@@ -1,5 +1,8 @@
-const calculateBabysittingFee = () => {
-  return 0;
-};
+const calculateBabysittingFee = (startTime, endTime, familyCode) => {
+  if (startTime === undefined ||  endTime === undefined || familyCode === undefined) {
+    throw new Error('Error: 3 arguments required, start time, end time and family code letter');
+  }
+  return 0.00;
+}
 
 export default calculateBabysittingFee;
